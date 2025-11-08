26325d68c9aedb6790b88becfff86da66df89f67
MariamYousry1234 <mariamyousry030@gmail.com>
2025-11-08 15:07:23 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Image_Slideshow_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.lblImageName = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.Color.Transparent;
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbImage.Location = new System.Drawing.Point(142, 103);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(518, 346);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // lblImageName
+            // 
+            this.lblImageName.AutoSize = true;
+            this.lblImageName.BackColor = System.Drawing.Color.Transparent;
+            this.lblImageName.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImageName.ForeColor = System.Drawing.Color.Yellow;
+            this.lblImageName.Location = new System.Drawing.Point(324, 488);
+            this.lblImageName.Name = "lblImageName";
+            this.lblImageName.Size = new System.Drawing.Size(80, 35);
+            this.lblImageName.TabIndex = 1;
+            this.lblImageName.Text = "None";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrevious.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrevious.ForeColor = System.Drawing.Color.Black;
+            this.btnPrevious.Location = new System.Drawing.Point(153, 566);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(142, 43);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(506, 566);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(132, 43);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label
+            // 
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Font = new System.Drawing.Font("Trebuchet MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.Color.Lime;
+            this.label.Location = new System.Drawing.Point(322, 36);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(56, 46);
+            this.label.TabIndex = 4;
+            this.label.Text = "n.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(221, 488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumber.Font = new System.Drawing.Font("Trebuchet MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.ForeColor = System.Drawing.Color.Yellow;
+            this.lblNumber.Location = new System.Drawing.Point(384, 36);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(56, 46);
+            this.lblNumber.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Image_Slideshow_Project.Properties.Resources._1111;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 646);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.lblImageName);
+            this.Controls.Add(this.pbImage);
+            this.Name = "Form1";
+            this.Text = "Image Slideshow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Label lblImageName;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNumber;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Image_Slideshow_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+        string[] Images;
+        int Counter = 0;
+        byte TimerCounter = 0;
+
+        void ShowImage()
+        {
+            try
+            {
+                //Remove Image From Memory
+                if (pbImage.Image != null)
+                    pbImage.Image.Dispose();
+
+                pbImage.Image = Image.FromFile(Images[Counter]);
+                lblImageName.Text = Path.GetFileNameWithoutExtension(Images[Counter]);
+                lblNumber.Text = Convert.ToString(Counter + 1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Image display error: {ex.Message} ","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            btnNext.Enabled = Images.Length > 0;
+            btnPrevious.Enabled = Images.Length > 0;
+        }
+
+        void LoadImages()
+        {
+            FolderBrowserDialog ImagesFolder = new FolderBrowserDialog();
+
+            if (ImagesFolder.ShowDialog() == DialogResult.OK)
+            {
+                Images = Directory.GetFiles(ImagesFolder.SelectedPath, "*.jpg")
+                    .Concat(Directory.GetFiles(ImagesFolder.SelectedPath, "*.png")).ToArray();
+
+                ShowImage();
+              
+            }
+        }
+
+
+        void ShowNextImage()
+        {
+            if (Images.Length == ++Counter)
+                Counter = 0;
+          
+            ShowImage();
+            TimerCounter = 0;
+        }
+
+        void ShowPreviousImage()
+        {
+            if (--Counter < 0)
+                Counter = Images.Length-1;
+            
+            ShowImage();
+            TimerCounter = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadImages();
+            timer1.Start();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            ShowNextImage();
+
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            ShowPreviousImage();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(++TimerCounter == 10)
+                ShowNextImage();
+         
+        }
+    }
+}
